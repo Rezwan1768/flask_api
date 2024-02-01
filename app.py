2e16eccf953127de13bf73d9637937b5e4d3c6fb
Rezwan1768 <rezwan1768@gmail.com>
2024-01-31 19:02:01 -0500

    Added request for specific movies

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-movies = [
+movie_list = [
     {
         'id': 1,
         'director': 'Christopher Nolan',
@@ -42,28 +42,57 @@ movies = [
 ]
 
 @app.route('/movies', methods=['GET', 'POST'])
-def movie_list():
+def movies():
     if request.method == 'GET':
-        if len(movies) > 0:
-            return jsonify(movies)
+        if len(movie_list) > 0:
+            return jsonify(movie_list)
         else:
-            return 'No Movies Found', 404  # Added 'return'
-
+            return 'No Movies Found', 404  
+        
     if request.method == 'POST':
-        m_id = movies[-1]['id'] + 1
-        m_author = request.form['author']
+        m_id = movie_list[-1]['id'] + 1
+        m_director = request.form['director']  
         m_lang = request.form['language']
         m_title = request.form['title']
 
         new_movie = {
             'id': m_id,
-            'author': m_author,
+            'director': m_director,
             'language': m_lang,
             'title': m_title,
         }
 
-        movies.append(new_movie)
-        return jsonify(movies), 201
+        movie_list.append(new_movie)
+        return jsonify(movie_list), 201
     
+@app.route('/movies/<int:id>', methods=['GET', 'PUT', 'DELETE'])  
+def single_movie(id):
+    if request.method == 'GET':
+        for movie in movie_list:
+            if movie['id'] == id:
+                return jsonify(movie)
+
+    if request.method == "PUT":
+        for movie in movie_list:
+            if movie['id'] == id:
+                movie['director'] = request.form['director']
+                movie['language'] = request.form['language']
+                movie['title'] = request.form['title']  
+                
+                updated_movie = {
+                    'id': id,
+                    'director': movie['director'],
+                    'language': movie['language'],
+                    'title': movie['title'],
+                }
+                return jsonify(updated_movie)
+            
+    if request.method == "DELETE":
+        for index, movie in enumerate(movie_list):
+            if movie['id'] == id:
+                movie_list.pop(index)
+                return jsonify(movie_list)
+            
+
 if __name__ == "__main__":
     app.run()
